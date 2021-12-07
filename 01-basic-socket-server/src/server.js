a6f0b20adce2a45fb7dbf90ff009b5f650aa7dc5
Vic Flores <vicsito2014@gmail.com>
2021-12-07 11:01:09 -0600

    cors config  was changed v6

--- 01-basic-socket-server/src/server.js
+++ 01-basic-socket-server/src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
-const cors = require('cors');
 
 const Sockets = require('./socket');
 
@@ -10,26 +9,15 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
-    this.whitelist = [
-      'http://localhost:4000',
-      'https://socket-serv-react.herokuapp.com/',
-    ];
-
-    this.options = {
-      origin: (origin, callback) => {
-        if (this.whitelist.includes(origin) || !origin) {
-          callback(null, true);
-        } else {
-          callback(new Error('no permitido'));
-        }
-      },
-    };
 
     this.server = http.createServer(this.app);
 
     this.io = socketio(this.server, {
       cors: {
-        origin: this.options,
+        origin: [
+          'http://localhost:4000',
+          'https://socket-serv-react.herokuapp.com/',
+        ],
         method: ['GET', 'POST'],
       },
     });
@@ -37,7 +25,6 @@ class Server {
 
   middleware() {
     this.app.use(express.static(path.resolve(__dirname, '../public')));
-    /* this.app.use(cors(this.options)); */
   }
 
   configurarSockets() {
